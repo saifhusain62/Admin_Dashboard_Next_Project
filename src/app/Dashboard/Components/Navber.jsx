c42830f6e87320d8d09d3c37ae6fcae90029496d
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2026-02-15 23:42:47 +0600

    Navber Update

--- src/app/Dashboard/Components/Navber.jsx
+++ src/app/Dashboard/Components/Navber.jsx
@@ -2,16 +2,26 @@
 import React from 'react'
 import { useState } from 'react'
 import Link from 'next/link'
-import user from '@/public/user.jpg'
 import Image from 'next/image'
 
+export default function Navber({setIsOpen}) {
+  const [isUserMenuOpen,setUserMenu]=useState(false);
+  const [isNotificationOpen,setNotificationOpen]=useState(false);
 
-const Navber = () => {
+  const toggleUserMenu = ()=>{
+    setUserMenuOpen(!isUserMenuOpen);
+    setNotificationOpen(false);
+  };
+
+  const toggleNotification = ()=>{
+    setNotificationOpen(!isNotificationOpen);
+    setUserMenuOpen(false);
+  };
   return (
     <>
-      
+      <div className="bg-shite shadow-lg flex justify-between items-center gap-3"></div>
     </>
   )
 }
 
-export default Navber
+

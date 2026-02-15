786f99c48df72736cc72de97382ade68a64f99df
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2026-02-15 22:31:48 +0600

    Title Icon updated Done

--- src/app/Dashboard/Components/Navber.jsx
+++ src/app/Dashboard/Components/Navber.jsx
@@ -1,8 +1,16 @@
+"use client"
 import React from 'react'
+import { useState } from 'react'
+import Link from 'next/link'
+import user from '@/public/user.jpg'
+import Image from 'next/image'
+
 
 const Navber = () => {
   return (
-    <div>Navber</div>
+    <>
+      
+    </>
   )
 }
 
--- src/app/Dashboard/Page.jsx
+++ src/app/Dashboard/Page.jsx
@@ -11,6 +11,19 @@ function DashboardPage() {
     <div className='flex min-h-[100vh] h-[100vh] overflow-y-hidden'>
      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen}/>
     </div>
+    {isOpen &&(
+      <div className='fixed inset-0 bg-black/20 z-40 lg:hidden'
+      onClick={()=>setIsOpen(false)}>
+
+      </div>
+    )}
+    <div className='flex-1 bg-gray-100 h-[100vh] min-h-[100vh] overflow-y-scroll'>
+      <Navber setIsOpen={setIsOpen}/>
+      <div className='main-content p-5 bg-[#f3f8fe]'>
+        <Main/>
+      </div>
+
+    </div>
    </>
   )
 }

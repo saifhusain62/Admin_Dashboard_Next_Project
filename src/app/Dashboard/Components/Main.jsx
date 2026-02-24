47a94877b496cb20896b6e9d67b58e262d3dda4a
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2026-02-24 16:38:24 +0600

    Dashboard first done

--- src/app/Dashboard/Components/Main.jsx
+++ src/app/Dashboard/Components/Main.jsx
@@ -21,6 +21,7 @@ import recentcourse5 from '../Image/RecentCourse-5.png'
 import recentcourse6 from '../Image/RecentCourse-6.png'
 import { IoMdStar } from "react-icons/io";
 import dynamic from 'next/dynamic'
+import Image from 'next/image'
 
 const CourseData = [
     
@@ -35,21 +36,21 @@ const CourseData = [
 ];
 
 const RecentCourseData = [
-  {id:1 , name:"Vuejs Course" , time: "5hr 45 min", lesson:10, student: 65 , img:recentcourse1},
-  {id:2 , name:"Swift Course" , time: "6hr 45 min", lesson:10, student: 65 , img:recentcourse2},
-  {id:3 , name:"C Course" , time: "9hr 45 min", lesson:10, student: 65 , img:recentcourse3},
-  {id:4 , name:"Nodejs Course" , time: "1hr 45 min", lesson:10, student: 65 , img:recentcourse4},
-  {id:5 , name:"Django Course" , time: "2hr 45 min", lesson:10, student: 65 , img:recentcourse5},
-  {id:6 , name:"CSS3 Course" , time: "8hr 45 min", lesson:10, student: 65 , img:recentcourse6},
+  {id:1 , name:"Vuejs Course" , time: "5hr 45 min", lesson:10, student: 65 , Image:recentcourse1},
+  {id:2 , name:"Swift Course" , time: "6hr 45 min", lesson:10, student: 65 , Image:recentcourse2},
+  {id:3 , name:"C Course" , time: "9hr 45 min", lesson:10, student: 65 , Image:recentcourse3},
+  {id:4 , name:"Nodejs Course" , time: "1hr 45 min", lesson:10, student: 65 , Image:recentcourse4},
+  {id:5 , name:"Django Course" , time: "2hr 45 min", lesson:10, student: 65 , Image:recentcourse5},
+  {id:6 , name:"CSS3 Course" , time: "8hr 45 min", lesson:10, student: 65 , Image:recentcourse6},
 ];
 
 const instructorData = [
-  {id:1, name:"Leis Borman" , courses:17 , student:2134, reviews:"4.8(55K+ Students)", img:instructorImage1},
-  {id:2, name:"Saint Oshiea" , courses:10 , student:4567, reviews:"4.8(55K+ Students)", img:instructorImage2},
-  {id:3, name:"Jhon Borman" , courses:4 , student:1242, reviews:"4.8(55K+ Students)", img:instructorImage3},
-  {id:4, name:"Ilisshe Johman" , courses:8 , student:6732, reviews:"4.8(55K+ Students)", img:instructorImage4},
-  {id:5, name:"Katish Borman" , courses:26 , student:2341, reviews:"4.8(55K+ Students)", img:instructorImage5},
-  {id:6, name:"Luta Borman" , courses:20 , student:9826, reviews:"4.8(55K+ Students)", img:instructorImage6},
+  {id:1, name:"Leis Borman" , courses:17 , student:2134, reviews:"4.8(55K+ Students)", Image:instructorImage1},
+  {id:2, name:"Saint Oshiea" , courses:10 , student:4567, reviews:"4.8(55K+ Students)", Image:instructorImage2},
+  {id:3, name:"Jhon Borman" , courses:4 , student:1242, reviews:"4.8(55K+ Students)", Image:instructorImage3},
+  {id:4, name:"Ilisshe Johman" , courses:8 , student:6732, reviews:"4.8(55K+ Students)", Image:instructorImage4},
+  {id:5, name:"Katish Borman" , courses:26 , student:2341, reviews:"4.8(55K+ Students)", Image:instructorImage5},
+  {id:6, name:"Luta Borman" , courses:20 , student:9826, reviews:"4.8(55K+ Students)", Image:instructorImage6},
 ];
 
 const Main = () => {
@@ -58,7 +59,86 @@ const Main = () => {
 
   return (
     <>
+      <h2 className='sont-semibold text-2xl pb-5'>Dashboard !</h2>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 mb-5'>
+        <div className='p-5 rounded-xl bg-white w-full flex justify-between items-start gap-3 shadow-xl'>
+          <div>
+            <p className='text-gray-600 text-md font-normal'>Total Courses</p>
+            <h6 className='text-gray-600 text-2xl font-medium pb-3'>2000+</h6>
+            <Link href='/' className='text-blue-500 underline text-xs font-medium cursor-pointer'>View All</Link>
+          </div>
+          <div className='dashboard-icon bg-blue-500 min-w-10 min-h-10 flex justify-center items-center rounded-full cursor-pointer'>
+            <Image src={dashboardicon1} alt="dashbord-icon" className='object-cover' />
 
+          </div>
+
+        </div>
+         <div className='p-5 rounded-xl bg-white w-full flex justify-between items-start gap-3 shadow-xl'>
+          <div>
+            <p className='text-gray-600 text-md font-normal'>Enrolled Courses</p>
+            <h6 className='text-gray-600 text-2xl font-medium pb-3'>9000+</h6>
+            <Link href='/' className='text-blue-500 underline text-xs font-medium cursor-pointer'>View All</Link>
+          </div>
+          <div className='dashboard-icon bg-blue-500 min-w-10 min-h-10 flex justify-center items-center rounded-full cursor-pointer'>
+            <Image src={dashboardicon2} alt="dashbord-icon" className='object-cover' />
+
+          </div>
+
+        </div>
+
+         <div className='p-5 rounded-xl bg-white w-full flex justify-between items-start gap-3 shadow-xl'>
+          <div>
+            <p className='text-gray-600 text-md font-normal'>Active Courses</p>
+            <h6 className='text-gray-600 text-2xl font-medium pb-3'>600+</h6>
+            <Link href='/' className='text-blue-500 underline text-xs font-medium cursor-pointer'>View All</Link>
+          </div>
+          <div className='dashboard-icon bg-blue-500 min-w-10 min-h-10 flex justify-center items-center rounded-full cursor-pointer'>
+            <Image src={dashboardicon3} alt="dashbord-icon" className='object-cover' />
+
+          </div>
+
+        </div>
+
+         <div className='p-5 rounded-xl bg-white w-full flex justify-between items-start gap-3 shadow-xl'>
+          <div>
+            <p className='text-gray-600 text-md font-normal'>Complete Courses</p>
+            <h6 className='text-gray-600 text-2xl font-medium pb-3'>100+</h6>
+            <Link href='/' className='text-blue-500 underline text-xs font-medium cursor-pointer'>View All</Link>
+          </div>
+          <div className='dashboard-icon bg-blue-500 min-w-10 min-h-10 flex justify-center items-center rounded-full cursor-pointer'>
+            <Image src={dashboardicon4} alt="dashbord-icon" className='object-cover' />
+
+          </div>
+
+        </div>
+
+         <div className='p-5 rounded-xl bg-white w-full flex justify-between items-start gap-3 shadow-xl'>
+          <div>
+            <p className='text-gray-600 text-md font-normal'>Total Student</p>
+            <h6 className='text-gray-600 text-2xl font-medium pb-3'>89,000+</h6>
+            <Link href='/' className='text-blue-500 underline text-xs font-medium cursor-pointer'>View All</Link>
+          </div>
+          <div className='dashboard-icon bg-blue-500 min-w-10 min-h-10 flex justify-center items-center rounded-full cursor-pointer'>
+            <Image src={dashboardicon5} alt="dashbord-icon" className='object-cover' />
+
+          </div>
+
+        </div>
+
+         <div className='p-5 rounded-xl bg-white w-full flex justify-between items-start gap-3 shadow-xl'>
+          <div>
+            <p className='text-gray-600 text-md font-normal'>Total Earnings</p>
+            <h6 className='text-gray-600 text-2xl font-medium pb-3'>110,000$</h6>
+            <Link href='/' className='text-blue-500 underline text-xs font-medium cursor-pointer'>View All</Link>
+          </div>
+          <div className='dashboard-icon bg-blue-500 min-w-10 min-h-10 flex justify-center items-center rounded-full cursor-pointer'>
+            <Image src={dashboardicon6} alt="dashbord-icon" className='object-cover' />
+
+          </div>
+
+        </div>
+
+      </div>
     </>
   )
 }
